2d8f815687e6e3b9fe29388d31fbc3950699d1e3
Ahmed Farouk <ahmedmohamedfarouk212103048@gmail.com>
2025-03-12 10:17:45 +0200

    firstt commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PinterrestApp());
+}
+
+class PinterrestApp extends StatelessWidget {
+  const PinterrestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
